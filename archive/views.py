b6d0d945fd5c3f2af48f6153c8968e1465f12b50
fr4ncad <78029942+fr4ncad@users.noreply.github.com>
2021-04-26 21:37:31 +0200

    i suggest you do these

--- archive/views.py
+++ archive/views.py
@@ -12,6 +12,14 @@ def signup(request):
             return render(request, 'archives.html', context)
     return render(request, 'login_page.html')
 
+def signin(request):
+    context = {'felhasznalok':user.objects.all()}
+    if request.method == 'POST':
+        if user.form(request.POST):
+            user.objects.create(nev=POST["name"], jelszo=["password"], anonymus=hash(POST["name"],POST["password"])
+            return render(request, 'archives.html', context)
+    return render(request, 'login_page.html')
+
 def bejegyzes(request):
     content = {'felhasznalonev':cu}
     return render(request, 'content.html')
